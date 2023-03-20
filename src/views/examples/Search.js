985b674712fce6a925399b476327852b7c4bfe80
GeonBbang <gun4930@naver.com>
2023-03-20 14:54:20 +0900

    chore(Search): 검색한 제목 state로 관리 및 promise 함수 수정

--- src/views/examples/Search.js
+++ src/views/examples/Search.js
@@ -65,30 +65,32 @@ const BookInfoViewList = ({ bookList }) => {
 	);
 };
 
+const promiseFunc = (name, num) => {
+	return new Promise(res => {
+		setTimeout(() => {
+			res(
+				Array.from({ length: 13 }, (_, i) => ({
+					title: `${name}${i * num}`,
+					authors: `저자${i * num}`,
+					isbn: i,
+					thumbnail: 'https://image.yes24.com/momo/TopCate0001/kepub/L_195737.jpg',
+					total_ratin: ((i + 1) / 3).toFixed(2),
+				})),
+			);
+		}, 1000);
+	});
+};
+
 const Search = () => {
 	const mainRef = useRef(null);
 	const [bookList, setBookList] = useState([]);
 	const [curPage, setCurPage] = useState(1);
+	const [bookName, setBookName] = useState('');
 
-	const onSubmitTitle = async () => {
+	const onSubmitTitle = async name => {
 		try {
-			const promiseFunc = () => {
-				return new Promise(res => {
-					setTimeout(() => {
-						res(
-							Array.from({ length: 13 }, (_, i) => ({
-								title: `타이틀${i}`,
-								authors: `저자${i}`,
-								isbn: i,
-								thumbnail: 'https://image.yes24.com/momo/TopCate0001/kepub/L_195737.jpg',
-								total_ratin: ((i + 1) / 3).toFixed(2),
-								star_cnt: Math.floor((i + 1) / 3),
-							})),
-						);
-					}, 1000);
-				});
-			};
-			const result = await promiseFunc();
+			setBookName(name);
+			const result = await promiseFunc(name, curPage);
 			setBookList(result);
 		} catch (e) {
 			console.log(e);
@@ -97,24 +99,9 @@ const Search = () => {
 
 	const onClickPagination = async num => {
 		try {
-			const promiseFunc = () => {
-				return new Promise(res => {
-					setTimeout(() => {
-						res(
-							Array.from({ length: 13 }, (_, i) => ({
-								title: `타이틀${i}`,
-								authors: `저자${i}`,
-								isbn: i,
-								thumbnail: 'https://image.yes24.com/momo/TopCate0001/kepub/L_195737.jpg',
-								total_ratin: ((i + 1) / 3).toFixed(2),
-								star_cnt: Math.floor((i + 1) / 3),
-							})),
-						);
-					}, 1000);
-				});
-			};
-			await promiseFunc();
 			setCurPage(num);
+			const result = await promiseFunc(bookName, num);
+			setBookList(result);
 		} catch (e) {
 			console.log(e);
 		}
@@ -124,7 +111,7 @@ const Search = () => {
 		document.documentElement.scrollTop = 0;
 		document.scrollingElement.scrollTop = 0;
 		mainRef.current.scrollTop = 0;
-	}, []);
+	}, [curPage]);
 
 	return (
 		<>
@@ -149,7 +136,7 @@ const Search = () => {
 										<Form
 											onSubmit={e => {
 												e.preventDefault();
-												onSubmitTitle();
+												onSubmitTitle(e.target[0].value);
 											}}
 										>
 											<FormGroup className="mb-3">
