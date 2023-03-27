e3d7000c29831cc7b295e4ff8f742e6a423b60b4
GeonBbang <gun4930@naver.com>
2023-03-27 14:34:01 +0900

    chore(Detail): API 명세서에 따른 데이터 수정

--- src/views/Detail.js
+++ src/views/Detail.js
@@ -14,6 +14,7 @@ import {
 import { Rating } from 'react-simple-star-rating';
 import { useRecoilValue } from 'recoil';
 import NotificationAlert from 'react-notification-alert';
+import axios from 'axios';
 import { userState } from 'recoils/user';
 import { notify } from 'util';
 
@@ -21,22 +22,25 @@ const bookInfoPromiseFunc = id => {
 	return new Promise(res => {
 		setTimeout(() => {
 			res({
-				title: `책이름${id}`,
-				authors: `저자${id}`,
+				title: `혼자 공부하는 파이썬${id}`,
+				authors: `윤인성${id}`,
+				contents: `혼자 공부하는 파이썬』이 더욱 흥미있고 알찬 내용으로 개정되었습니다. 프로그래밍이 정말 처음인 입문자도 따라갈 수 있는 친절한 설명과 단계별 학습은 그대로! 혼자 
+공부하더라도 체계적으로 계획을 세워 학습할 수 있도록 ‘혼공 계획표’를 새롭게 추가했습니다. 또한 입문자가 자주 물어보는 질문과 오류 해결 방법을 적재적소에 배치하여 예상치 못한 문제에 부딪혀도 좌절하지  
+않고 끝까지 완독할 수 있도록 도와줍니다. 단순한 문법 암기와 코딩 따라하기에 지쳤다면, 새로운,`,
+				thumbnail:
+					'https://search1.kakaocdn.net/thumb/R120x174.q85/?fname=http%3A%2F%2Ft1.daumcdn.net%2Flbook%2Fimage%2F6080832%3Ftimestamp%3D20230322161657',
+				url: 'https://search.daum.net/search?w=bookpage&bookId=6080832&q=%ED%98%BC%EC%9E%90+%EA%B3%B5%EB%B6%80%ED%95%98%EB%8A%94+%ED%8C%8C%EC%9D%B4%EC%8D%AC',
 				isbn: id,
-				thumbnail: 'https://image.yes24.com/momo/TopCate0001/kepub/L_195737.jpg',
-				total_ratin: 3.33,
-				content:
-					'내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내',
+				rate: 3.33,
 				myReview: { rating: 0.5, comment: '댓글댓글댓글댓글댓글댓글댓글댓글' },
-				commentList: Array.from({ length: 10 }, (_, i) => ({
+				review: Array.from({ length: 10 }, (_, i) => ({
 					id: `id${i}`,
-					comment: `댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글${i}`,
-					rating: 3.5,
+					content: `댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글${i}`,
+					rate: 3.5,
 					like: i * 2,
 					dislike: i,
-					/** 눌렀다면 어디에 눌렀는지 1이면 like 2이면 dislike 이 값 0이라면 아직 안누름*/
-					userCheck: i % 3,
+					/** 1: 안누름, 2: 좋아요, 3: 싫어요 */
+					userCheck: (i % 3) + 1,
 				})),
 			});
 		}, 1000);
@@ -82,15 +86,15 @@ const OtherReview = ({ reviewList, onClickLike }) => {
 		<>
 			{reviewList.length ? (
 				reviewList
-					.filter(review => review.id !== userId)
-					.map((review, i) => (
+					.filter(userReview => userReview.id !== userId)
+					.map((userReview, i) => (
 						<Card key={i} className="shadow shadow-lg--hover mt-5">
 							<CardBody>
 								<div className="d-flex">
 									<div className="pl-4 md text-left">
-										{review.id}{' '}
+										{userReview.id}{' '}
 										<Badge color="primary" pill className="mr-1">
-											Rating: {review.rating}
+											Rating: {userReview.rate}
 										</Badge>
 										<Badge
 											color="danger"
@@ -102,10 +106,10 @@ const OtherReview = ({ reviewList, onClickLike }) => {
 											style={{ cursor: 'pointer' }}
 										>
 											<i
-												style={{ color: review.userCheck === 1 ? 'black' : '' }}
+												style={{ color: userReview.userCheck === 2 ? 'black' : '' }}
 												className="ni ni-bold-up"
 											/>{' '}
-											{review.like}
+											{userReview.like}
 										</Badge>
 										<Badge
 											color="info"
@@ -117,10 +121,10 @@ const OtherReview = ({ reviewList, onClickLike }) => {
 											style={{ cursor: 'pointer' }}
 										>
 											<i
-												style={{ color: review.userCheck === 2 ? 'black' : '' }}
+												style={{ color: userReview.userCheck === 3 ? 'black' : '' }}
 												className="ni ni-bold-down"
 											/>{' '}
-											{review.dislike}
+											{userReview.dislike}
 										</Badge>
 										<hr
 											style={{
@@ -128,7 +132,7 @@ const OtherReview = ({ reviewList, onClickLike }) => {
 												marginBottom: '10px',
 											}}
 										/>
-										<p>{review.comment}</p>
+										<p>{userReview.content}</p>
 									</div>
 								</div>
 							</CardBody>
@@ -170,36 +174,47 @@ const Detail = ({ match }) => {
 		}
 	};
 
-	const onClickLike = async (isUp, idx) => {
+	const onClickLike = async (isLike, idx) => {
 		try {
-			const commentList = [...bookInfo?.commentList];
-			if (!commentList[idx]) {
+			const review = [...bookInfo?.review];
+			if (!review[idx]) {
 				throw Error('해당 인덱스가 없습니다');
 			}
-			await likePromiseFunc(commentList[idx].id, isUp);
+			await likePromiseFunc(review[idx].id, isLike);
 
-			const beforeLike = commentList[idx].userCheck;
-			if (beforeLike === 0) {
-				commentList[idx][isUp ? 'like' : 'dislike'] += 1;
+			const beforeLike = review[idx].userCheck;
+			review[idx].userCheck = isLike ? 2 : 3;
+			/** 아무것도 안누른 상태 */
+			if (beforeLike === 1) {
+				review[idx][isLike ? 'like' : 'dislike'] += 1;
 			} else {
-				commentList[idx][beforeLike === 1 ? 'like' : 'dislike'] -= 1;
+				/** 무언가가 눌려진 상태에서 기존의 상태를 제거 */
+				review[idx][beforeLike === 2 ? 'like' : 'dislike'] -= 1;
+				/** 좋아요에서 싫어요를 누른 경우 */
+				if (beforeLike === 2 && !isLike) {
+					review[idx].dislike += 1;
+				} else if (beforeLike === 3 && isLike) {
+					/** 싫어요에서 좋아요를 누른 경우 */
+					review[idx].like += 1;
+				} else if ((beforeLike === 2 && isLike) || (beforeLike === 3 && !isLike)) {
+					/** 같은 버튼을 연속해서 누른 경우 */
+					review[idx].userCheck = 1;
+				}
 			}
-			commentList[idx].userCheck = isUp ? 1 : 2;
 
-			if (beforeLike === 1 && !isUp) {
-				commentList[idx].dislike += 1;
-			} else if (beforeLike === 2 && isUp) {
-				commentList[idx].like += 1;
-			} else if (beforeLike !== 0) {
-				commentList[idx].userCheck = 0;
-			}
-			setBookInfo({ ...bookInfo, commentList });
+			/** 아무것도 안눌렸던 상태에서 싫어요인 경우  */
+			setBookInfo({ ...bookInfo, review });
 		} catch (e) {
 			console.log(e);
 		}
 	};
 
 	useEffect(() => {
+		// axios.get(`http://......./book/${match.params.id}`,{withCredentials: true}).then(({data}) => {
+		// 	setBookInfo(data);
+		// 	commentRef.current.value = '123';
+		// 	setRating(data?.myReview?.rating ?? 0);
+		// });
 		bookInfoPromiseFunc(match.params.id).then(result => {
 			setBookInfo(result);
 			commentRef.current.value = '123';
@@ -237,7 +252,7 @@ const Detail = ({ match }) => {
 										marginBottom: '30px',
 									}}
 								/>
-								<p>{bookInfo.content}</p>
+								<p>{bookInfo.contents}</p>
 							</div>
 						</Col>
 					</Row>
@@ -276,11 +291,8 @@ const Detail = ({ match }) => {
 			</section>
 			<section className="section bg-secondary">
 				<Container>
-					<h2>리뷰 ({bookInfo.commentList?.length ?? 0})</h2>
-					<OtherReview
-						reviewList={bookInfo.commentList ?? []}
-						onClickLike={onClickLike}
-					/>
+					<h2>리뷰 ({bookInfo.review?.length ?? 0})</h2>
+					<OtherReview reviewList={bookInfo.review ?? []} onClickLike={onClickLike} />
 				</Container>
 			</section>
 		</>
