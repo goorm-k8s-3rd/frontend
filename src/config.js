efb67bea75e8b0dc484c8d2bde150fb7a80efc80
hsh108868 <pusrty@gmail.com>
2023-04-11 16:33:23 +0900

    modify config.js

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 export const api =
-	'https://k8s-booknetw-webingre-70046c2109-769394042.ap-northeast-2.elb.amazonaws.com';
+	'http://k8s-booknetw-webingre-70046c2109-769394042.ap-northeast-2.elb.amazonaws.com';
